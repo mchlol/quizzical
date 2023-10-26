413f15ff5ed1f26269ae5bb275f53053996a3499
Michelle Brown <m.andreabr@gmail.com>
2023-10-26 19:31:46 +1100

    get an array of correct answers, return the users selections from the from data, compare and return an array of booleans

--- components/Question.js
+++ components/Question.js
@@ -4,9 +4,18 @@ import {decode} from 'html-entities';
 
 export default function Question(props) {
 
-    const question = props.question;
-    console.log('Question: ', question);
+    const [question, setQuestion] = React.useState(props.question);
+    const [shuffled, setShuffled] = React.useState(shuffleAnswers(question.incorrect_answers, question.correct_answer));
 
+    // TODO: for debugging only - remove before build
+    // React.useEffect( () => {
+    //     console.log('Question: ', question);
+    //     console.log('Correct: ', correct);
+    //     console.log('Shuffled: ', shuffled);
+
+    // }, [question]);
+
+    // TODO: move 2 functions below into a utils file
     function shuffleAnswers(incorrectArr, correct) {
         /* 
         get the array of incorrect answers and the correct answers
@@ -25,13 +34,26 @@ export default function Question(props) {
     return (
         <div>
             
-            <h3 className="question">Question</h3>
+            <h3 className="question">{decodeText(question.question)}</h3>
             
             <div className="answers-container">
-                <label htmlFor="answer">
-                    <input type="radio" id="answer" name="question" value="answer" /> Answer
+                <label htmlFor={shuffled[0]}>
+                    <input type="radio" id={shuffled[0]} name={question.question} value={shuffled[0]} defaultChecked={true}/> {decode(shuffled[0])}
+                </label>
+
+                <label htmlFor={shuffled[1]}>
+                    <input type="radio" id={shuffled[1]} name={question.question} value={shuffled[1]} /> {decode(shuffled[1])}
+                </label>
+
+                <label htmlFor={shuffled[2]}>
+                    <input type="radio" id={shuffled[2]} name={question.question} value={shuffled[2]} /> {decode(shuffled[2])}
+                </label>
+
+                <label htmlFor={shuffled[3]}>
+                    <input type="radio" id={shuffled[3]} name={question.question} value={shuffled[3]} /> {decode(shuffled[3])}
                 </label>
             </div>
+
             <hr/>
         </div>
     )
--- components/Quiz.js
+++ components/Quiz.js
@@ -4,14 +4,70 @@ import Question from "./Question";
 export default function Quiz(props) {
 
     const questions = props.questions;
-    console.log('Quiz questions: ', questions); // an array of objects
+    
+
+    // copy the object with a new property
+    // TODO: look into why this can't be done
+    // const [question0, setQuestion0] = React.useState( { ...questions[0], key: "value" });
+
+    // TODO: if these won't be used, remove before build
+    // const [question0, setQuestion0] = React.useState(questions[0]);
+    // const [question1, setQuestion1] = React.useState(questions[1]);
+    // const [question2, setQuestion2] = React.useState(questions[2]);
+    // const [question3, setQuestion3] = React.useState(questions[3]);
+    // const [question4, setQuestion4] = React.useState(questions[4]);
+
+    // console.table('Question0: ', question0)
+
+    const [quizAnswers, setQuizAnswers] = React.useState([]);
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(e);
+        const form = e.target;
+        const formData = new FormData(form);
+
+        const formJson = Object.fromEntries(formData.entries());
+        // returns question: answer key value pairs as strings only - no other info
+        setQuizAnswers(formJson);
+        console.log('quizAnswers: ', quizAnswers);
+
+    } // handleSubmit
+
+    function checkAnswers(data) {
+        
+        // extract all the correct answers from each question object
+        const correctAnswers = [];
+        for (const question of questions) {
+            correctAnswers.push(question.correct_answer);
+        }
+        // store the user answers returned from the from
+        const userAnswers = Object.values(data);
+
+        console.log('User answers: ',userAnswers)
+        console.log('Correct answers: ', correctAnswers);
+
+        // loop through the user answers and compare to the correct answers
+
+        const results = [];
+        for (let i = 0; i < userAnswers.length; i++) {
+            if (userAnswers[i] === correctAnswers[i]) {
+                results.push(true);
+            } else {
+                results.push(false)
+            }
+        };
+
+        console.log('Results: ', results);
+        // return an array of true or false in each corresponding position
+        return results;
+
+        
     }
 
-    // create a Question element (component), where props will be a single question object
+    React.useEffect( () => {
+        checkAnswers(quizAnswers);
+    }, [quizAnswers]);
+
 
     return (
 
@@ -19,7 +75,12 @@ export default function Quiz(props) {
         className="questions-container flex-centered"
         onSubmit={handleSubmit}>
 
-            <Question question={questions[0]}/>
+            <Question question={questions[0]} />
+            <Question question={questions[1]} />
+            <Question question={questions[2]} />
+            <Question question={questions[3]} />
+            <Question question={questions[4]} />
+
 
             <button className="btn action-btn">Check answers</button>
 
