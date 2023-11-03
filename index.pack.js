8520ee0464d8ab2afa272e987f7167b303f6c2a7
Michelle Brown <m.andreabr@gmail.com>
2023-11-03 16:12:24 +1100

    add and style blobs

--- index.pack.js
+++ index.pack.js
@@ -2602,6 +2602,16 @@ function App() {
     return _react2.default.createElement(
         "div",
         null,
+        _react2.default.createElement(
+            "svg",
+            { className: "blob-large", width: "269", height: "273", viewBox: "0 0 269 273", fill: "none", xmlns: "http://www.w3.org/2000/svg" },
+            _react2.default.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M62.9973 213.544C34.7091 183 -3.0943 153.931 0.760539 112.456C5.0272 66.5503 39.4419 26.7905 82.0069 9.01646C122.385 -7.84434 169.623 1.89353 205.409 27C235.535 48.1352 236.41 88.2738 245.729 123.878C255.758 162.2 282.109 202.96 260.089 235.929C237.126 270.308 188.579 275.581 147.519 270.918C111.906 266.873 87.3383 239.826 62.9973 213.544Z", fill: "#FFFAD1" })
+        ),
+        _react2.default.createElement(
+            "svg",
+            { className: "blob-small", width: "297", height: "235", viewBox: "0 0 297 235", fill: "none", xmlns: "http://www.w3.org/2000/svg" },
+            _react2.default.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M143.448 4.90596C184.961 1.77498 231.243 -9.72149 261.306 19.1094C294.581 51.0203 304.282 102.703 291.701 147.081C279.767 189.18 242.745 220.092 200.821 232.476C165.528 242.902 133.567 218.605 99.8993 203.738C63.6625 187.737 15.3588 182.993 3.25932 145.239C-9.35799 105.868 16.7305 64.5881 45.9358 35.3528C71.2672 9.99541 107.727 7.60006 143.448 4.90596Z", fill: "#DEEBF8" })
+        ),
         start ? _react2.default.createElement(_Quiz2.default, { start: start, setStart: setStart }) : _react2.default.createElement(
             "div",
             { className: "flex-centered" },
@@ -4511,7 +4521,7 @@ function Question(props) {
         null,
         _react2.default.createElement(
             'h3',
-            null,
+            { className: 'question' },
             (0, _utils.decodeText)(questionData.question)
         ),
         _react2.default.createElement(
@@ -4653,16 +4663,14 @@ function Quiz(props) {
     } // sendAnswer
 
     function checkAnswers() {
-        console.log('checking answers');
 
-        // check which answers were correct
         var scoreHolder = 0;
         var arr = [];
         for (var i = 0; i < Object.keys(correctAnswers).length; i++) {
 
             if (Object.values(selectedAnswers)[i] === Object.values(correctAnswers)[i]) {
                 arr.push(true);
-                scoreHolder = scoreHolder + 1;
+                scoreHolder++;
             } else {
                 arr.push(false);
                 // if any question was not answered its value will be just be false
@@ -4741,7 +4749,11 @@ function Quiz(props) {
             finished ? _react2.default.createElement(
                 "div",
                 { className: "end-div" },
-                _react2.default.createElement(
+                score === 5 ? _react2.default.createElement(
+                    "p",
+                    { className: "end-p" },
+                    "YOU GOT THEM ALL RIGHT! YOU ARE AMAZING!"
+                ) : _react2.default.createElement(
                     "p",
                     { className: "end-p" },
                     "You scored ",
